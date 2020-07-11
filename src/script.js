17843c7012bfad22fb954023d4aa5c83044d6141
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-07-11 18:26:46 +0300

    init

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import { load } from './js/utils';
+import { router } from './js/router';
+
+// window.addEventListener('hashchange', router);
+// window.onload = () => {
+//   load();
+//   router();
+// };
+renderDictionaryPage()
